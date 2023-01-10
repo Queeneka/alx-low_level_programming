25233a129945260990a9cd79a9a9f9707fd98c3c
Eka Endurance <queeneka111@gmail.com>
2023-01-10 22:00:48 +0100

    C - malloc, free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,20 +5,19 @@
  * @c: char to initialize the array with
  * Return: pointer to the array of chars
  */
- char *create_array(unsigned int size, char c)
- {
- char *p;
- unsigned int i;
- 
- if (size > 0)
- {
- p = malloc(size * sizeof(char));
- if (p ==NULL)
- return (NULL);
- }
- else
- return (NULL);
- for (i = 0; i < size; i++)
- p[i] = c;
- return (p);
- }
+char *create_array(unsigned int size, char c)
+{
+char *p;
+unsigned int i;
+if (size > 0)
+{
+p = malloc(size * sizeof(char));
+if (p == NULL)
+return (NULL);
+}
+else
+return (NULL);
+for (i = 0; i < size; i++)
+p[i] = c;
+return (p);
+}
