b790b1bf04d704ff30f2b4d4d3aecf36ed0e51a6
Eka Endurance <queeneka111@gmail.com>
2023-01-26 16:07:04 +0100

    Static libraries

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The int to print
+ * Return: 0
+ */
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -20,4 +28,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif
+
+#endif /* MAIN_H */
